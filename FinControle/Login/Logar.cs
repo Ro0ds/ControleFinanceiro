55415a5f230ca6c3bbeb6bf04fa44dc91e703252
Digo <digo_gabriel97@yahoo.com.br>
2022-06-02 14:38:42 -0300

    Add project files.

---
+++ FinControle/Login/Logar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using FirebirdSql.Data.FirebirdClient;
+using System.Text;
+
+namespace FinControle.Login {
+    class Logar {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string DicaSenha { get; set; }
+        public int Idade { get; set; }
+
+        public Logar(){}
+
+        public Logar(string nome, string senha, string dicaSenha, int idade) {
+            Nome = nome;
+            Senha = senha;
+            DicaSenha = dicaSenha;
+            Idade = idade;
+        }
+
+
+    }
+}
---
+++ FinControle/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FinControle.DB;
+using FinControle.Login;
+using System.Windows.Forms;
+
+namespace FinControle {
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            DBClassePrincipal dBClasse = new DBClassePrincipal();
+
+            dBClasse.VerificaBanco();
+        }
+    }
+}
